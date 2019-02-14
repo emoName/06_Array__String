581348398087424d4700ebf4ddf72cfb61db7c95
Ion Chiriac <ion.chiriac@amdaris.com>
2019-02-14 13:16:21 +0200

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Array__String
+{
+  static  class Class1
+    {
+
+      static  public void ShowArray(int[] a)
+        {
+
+            foreach (var b in a)
+            {
+                Console.WriteLine(b);
+            }
+
+        }
+       static public void ShowArrays(int [,] ss)
+        {
+
+            Console.WriteLine(ss.Rank);
+
+
+            for (int i = 0; i < ss.GetLength(0); i++)
+            {
+                Console.WriteLine(ss[i, 0]);
+
+                for (int j = 1; j < ss.GetLength(1); j++)
+                {
+                    Console.WriteLine(ss[i, j]);
+                }
+            }
+
+        }
+
+        static public void ShowArrays(int[][] ss)
+        {
+
+            //   Console.WriteLine(ss.Length);
+
+            for (int i = 0; i < ss.Length; i++)
+            {
+                ss[i] = new int[i+1];
+                Console.WriteLine("---------------------");
+                for (int j = 0; j < ss[i].Length; j++)
+                {
+                    ss[i][j] = j;
+                    Console.WriteLine(ss[i][j]);
+                }
+
+            }
+
+        }
+
+
+        static public void ModfStrings(string s)
+        {
+            for (int i = s.Length/2; i < s.Length/2+2; i++)
+            {
+                
+               s= s.Insert(i,"d") ;
+            }
+
+            Console.WriteLine(s);
+        }    
+
+
+        static public void SomeSplit(string ss)
+        {
+            string[] s;
+            s = ss.Split('g');
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i]);
+            }
+
+        }
+
+
+        }
+
+
+
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Array__String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = new int[] { 2,5,5,7};
+
+          //  Class1.ShowArray(a);
+
+            int[,] b = new int[,] { {2,5 },{6,8},{5,0} };
+
+          //  Class1.ShowArrays(b);
+
+            int[][] c = new int[4][];
+            //  Class1.ShowArrays(c);
+
+
+            //  Class1.ModfStrings("aaaaabbbbbbbb");
+
+            Class1.SomeSplit("uifgduiggugo");
+
+            
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
